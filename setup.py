c04c174d9fdcb5aff5ac2f7942b50893b1c1a288
Sujoy Nath <mityblur@gmail.com>
2024-05-25 18:07:19 +0530

    Pipeline Added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pipeline',  # Package name
+    version='0.1',  # Package version
+    packages=find_packages('pipeline'),  # Automatically find all packages
+)
